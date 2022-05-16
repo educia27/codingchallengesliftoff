8d7a294bac1877d21e714fa5fe3969d48c32255a
educia27 <edd1114@gmail.com>
2022-05-16 13:12:54 -0500

    coding practice

---
+++ CodeWarsPractice10.js
@@ -0,0 +1,12 @@
+function setAlarm(employed, vacation){
+  if (employed && vacation) {
+    return false;
+  }else if (!employed && vacation) {
+    return false;
+  } else if (!employed && !vacation) {
+    return false;
+  }else if (employed && !vacation) {
+    return true;
+  }
+}
+
