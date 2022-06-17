e44c902937be51dcc5397d156c7cf4dfa3f407cf
educia27 <edd1114@gmail.com>
2022-06-17 08:12:26 -0500

    remove vowels problem

---
+++ disemvowel.js
@@ -0,0 +1,10 @@
+function disemvowel(str) {
+    let vowels = ['a','e','i','o','u','A','E','I','O','U'];
+    let newStr = '';
+    for(let i = 0; i<str.length;i++){
+      if(!vowels.includes(str[i])){
+        newStr += str[i];
+      }
+    }
+    return newStr;
+  }
