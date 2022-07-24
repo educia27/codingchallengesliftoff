69441e6336c08a90b06f472c2873dd150a52bd00
educia27 <edd1114@gmail.com>
2022-07-24 10:52:01 -0500

    multiples of a number

---
+++ multiplesOfNumber.js
@@ -0,0 +1,12 @@
+function findMultiples(integer, limit) {
+  
+    let arr = [];
+    
+    for (let i = integer; i <= limit; i = i + integer) {
+        arr.push(i)
+    }
+    return arr;
+  
+}
+  
+  
