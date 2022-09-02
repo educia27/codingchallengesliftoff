3a9bfaaf24caedf91c578ad58da655fc6a014752
educia27 <edd1114@gmail.com>
2022-09-02 06:20:42 -0500

    divisor of integer N

---
+++ divisors.js
@@ -0,0 +1,11 @@
+function getDivisorsCnt(n){
+    let divisors = [];
+    
+      for (let i = 1; i <= n; i++) {
+        if ( n % i == 0) {
+          divisors.push(n);  
+        }
+      }
+  //   console.log(divisors)
+    return divisors.length;
+};
